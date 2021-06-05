90654e2c2a062904548f3a6c323b82a5a91b51ef
Korak Kurani <59537065+Korak-997@users.noreply.github.com>
2021-06-05 12:53:42 +0200

    Update main.js

--- main.js
+++ main.js
@@ -5,16 +5,40 @@ const prevBtn = document.getElementById('perv')
 const nextBtn = document.getElementById('next')
 
 let imgNum = 1
-prevBtn.addEventListener('click', ()=>{
-  imgNum = imgNum == 1 ? 8 : imgNum-=1
-  let nextImg = imgNum == 8 ? 1 : imgNum+1
-  body.style.backgroundImage = `url("./images/pic-${nextImg}.jpg")`
-  img.src=`./images/pic-${imgNum}.jpg`
+nextBtn.addEventListener('click', () => {
+	if(imgNum == 8){
+		imgNum = 1
+	}
+	else{
+		imgNum+=1
+	}
+	let nexImg = 1
+	if(imgNum == 8){
+		nextImg = 1
+	}
+	else{
+		nextImg = imgNum+1
+	}
+
+	body.style.backgroundImage = `url("./images/pic-${nextImg}.jpg")`
+	img.src=`./images/pic-${imgNum}.jpg`
 })
 
-nextBtn.addEventListener('click', ()=>{
-  imgNum = imgNum == 8 ? 1 : imgNum += 1
-  let nextImg = imgNum == 8 ? 1 : imgNum+1
-  body.style.backgroundImage = `url("./images/pic-${nextImg}.jpg")` 
-  img.src=`./images/pic-${imgNum}.jpg`
+prevBtn.addEventListener('click', ()=>{
+	if(imgNum == 1){
+		imgNum = 8
+	}
+	else{
+		imgNum-=1
+	}
+	let nexImg = 1
+	if(imgNum == 8){
+		nextImg = 1
+	}
+	else{
+		nextImg = imgNum+1
+	}
+
+	body.style.backgroundImage = `url("./images/pic-${nextImg}.jpg")`
+	img.src=`./images/pic-${imgNum}.jpg`
 })
